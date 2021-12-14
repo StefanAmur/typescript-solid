bca1eece50564a9f2b3165de3bd0c0e6428089f2
StefanAmur <stefan.amur22@gmail.com>
2021-12-14 16:14:45 +0100

    switched import from IAnimal to Animal

--- 1.O/Model/Zoo.ts
+++ 1.O/Model/Zoo.ts
@@ -1,9 +1,10 @@
-import { IAnimal } from './IAnimal';
+// import { IAnimal } from './IAnimal';
+import { Animal } from './Animal';
 
 export class Zoo {
   private _animals: Array<Object> = new Array<Object>();
 
-  public addAnimal(animal: IAnimal) {
+  public addAnimal(animal: Animal) {
     this._animals.push(animal);
   }
 
