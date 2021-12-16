49f3d08a5ec208c0cc7ed0949dcf09d052d1ee01
StefanAmur <stefan.amur22@gmail.com>
2021-12-16 09:11:48 +0100

    added IOven and renamed the methods

---
+++ 4.D/new.js
@@ -0,0 +1,77 @@
+var GasOven = /** @class */ (function () {
+    function GasOven() {
+    }
+    GasOven.prototype.turnOn = function () {
+        setTimeout(function () {
+            document.getElementById('target').innerHTML +=
+                '<p>' +
+                    new Date().getHours() +
+                    ':' +
+                    new Date().getMinutes() +
+                    ' : THE GAS IS ON!</p>';
+        }, 1000);
+        console.log('THE GAS IS ON!'); //insert fart humor here
+        this._isOn = true;
+    };
+    GasOven.prototype.turnOff = function () {
+        setTimeout(function () {
+            document.getElementById('target').innerHTML +=
+                '<p>' +
+                    new Date().getHours() +
+                    ':' +
+                    new Date().getMinutes() +
+                    ' : THE GAS IS OFF!</p><hr>';
+        }, 3000);
+        console.log('THE GAS IS OFF!'); //insert fart humor here
+        this._isOn = false;
+    };
+    GasOven.prototype.bake = function (item) {
+        if (this._isOn) {
+            setTimeout(function () {
+                document.getElementById('target').innerHTML +=
+                    '<p>' +
+                        new Date().getHours() +
+                        ':' +
+                        new Date().getMinutes() +
+                        ' : Now baking ' +
+                        item +
+                        ' !</p>';
+            }, 2000);
+            console.log('Now baking ' + item + '!');
+        }
+        else {
+            setTimeout(function () {
+                document.getElementById('target').innerHTML +=
+                    '<p>' +
+                        new Date().getHours() +
+                        ':' +
+                        new Date().getMinutes() +
+                        ' : there is no gas!</p>';
+            }, 2000);
+            console.log('there is no gas!'); //insert fart humor here
+        }
+    };
+    return GasOven;
+}());
+var Restaurant = /** @class */ (function () {
+    function Restaurant(name, oven) {
+        this._name = name;
+        this._oven = oven;
+    }
+    Restaurant.prototype.Cook = function (item) {
+        this._oven.turnOn();
+        this._oven.bake(item);
+        this._oven.turnOff();
+    };
+    return Restaurant;
+}());
+var bakery = new Restaurant('Bakery', new GasOven());
+bakery.Cook('cookies');
+//Now if we want to add a new restaurant with an ELECTRIC cooker, we are gonna be in a hot mess ...
+/*
+let bakery = new Restaurant("Bakery", new Oven());
+bakery.Cook("cookies");
+
+let crepery = new Restaurant("Crepery", new Stove());
+crepery.Cook("crepes");
+ */
---
+++ 4.D/new.ts
@@ -0,0 +1,89 @@
+interface IOven {
+  turnOn(): void;
+  turnOff(): void;
+  bake(item: string): void;
+}
+
+class GasOven implements IOven {
+  private _isOn: boolean;
+
+  public turnOn(): void {
+    setTimeout(function () {
+      document.getElementById('target').innerHTML +=
+        '<p>' +
+        new Date().getHours() +
+        ':' +
+        new Date().getMinutes() +
+        ' : THE GAS IS ON!</p>';
+    }, 1000);
+    console.log('THE GAS IS ON!'); //insert fart humor here
+    this._isOn = true;
+  }
+
+  public turnOff(): void {
+    setTimeout(function () {
+      document.getElementById('target').innerHTML +=
+        '<p>' +
+        new Date().getHours() +
+        ':' +
+        new Date().getMinutes() +
+        ' : THE GAS IS OFF!</p><hr>';
+    }, 3000);
+    console.log('THE GAS IS OFF!'); //insert fart humor here
+    this._isOn = false;
+  }
+
+  public bake(item: string) {
+    if (this._isOn) {
+      setTimeout(function () {
+        document.getElementById('target').innerHTML +=
+          '<p>' +
+          new Date().getHours() +
+          ':' +
+          new Date().getMinutes() +
+          ' : Now baking ' +
+          item +
+          ' !</p>';
+      }, 2000);
+      console.log('Now baking ' + item + '!');
+    } else {
+      setTimeout(function () {
+        document.getElementById('target').innerHTML +=
+          '<p>' +
+          new Date().getHours() +
+          ':' +
+          new Date().getMinutes() +
+          ' : there is no gas!</p>';
+      }, 2000);
+      console.log('there is no gas!'); //insert fart humor here
+    }
+  }
+}
+
+class Restaurant {
+  private _name: string;
+  private _oven: IOven;
+
+  constructor(name: string, oven: IOven) {
+    this._name = name;
+    this._oven = oven;
+  }
+
+  public Cook(item: string) {
+    this._oven.turnOn();
+    this._oven.bake(item);
+    this._oven.turnOff();
+  }
+}
+
+let bakery = new Restaurant('Bakery', new GasOven());
+bakery.Cook('cookies');
+
+//Now if we want to add a new restaurant with an ELECTRIC cooker, we are gonna be in a hot mess ...
+/*
+let bakery = new Restaurant("Bakery", new Oven());
+bakery.Cook("cookies");
+
+let crepery = new Restaurant("Crepery", new Stove());
+crepery.Cook("crepes");
+ */
